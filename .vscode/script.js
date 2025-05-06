c8c101e05daa44c053b13a88e9d46b563abb30c3
Cuba <sergio_cuba98@hotmail.com>
2025-05-05 23:30:38 -0300

    :rockandroll initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,16 @@
+function trocarDeModo() {
+  const html = document.documentElement
+  if (html.classList.contains("modoclaro")) {
+    html.classList.remove("modoclaro")
+  } else {
+    html.classList.add("modoclaro")
+  }
+
+  const img = document.querySelector("#perfil img")
+
+  if (html.classList.contains("modoclaro")) {
+    img.setAttribute("src", "./imagens/cubinha.png")
+  } else {
+    img.setAttribute("src", "./imagens/Perfilsemfundo.png")
+  }
+}
